c4a07c2199a4fa523178d69d7cdee7c888273420
Jess Cushnie <94955791+jmcushnie@users.noreply.github.com>
2023-08-21 20:12:47 +1200

    Environment variables
    store env variables in .env file

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const clientId = process.env.CLIENT_ID;
+const clientId = import.meta.env.VITE_CLIENT_ID;
 const code = undefined;
 
+console.log(clientId);
 if (!code) {
   redirectToAuthCodeFlow(clientId);
 } else {
