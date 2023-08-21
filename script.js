e0f1aa685c28fc65c509f1ae6edb9694894ad9aa
Jess Cushnie <94955791+jmcushnie@users.noreply.github.com>
2023-08-21 20:31:41 +1200

    Spotify authorisation using PKCE flow
    Implemented function that generates a random code verifier and its corresponding challenge, stores the verifier locally, and constructs the authorisation URL

--- script.js
+++ script.js
@@ -10,8 +10,46 @@ if (!code) {
   populateUI(profile);
 }
 
-async function redirectToAuthCodeFlow(clientId) {
-  // TODO: Redirect to Spotify authorization page
+// Authorisation code flow Spotify using PKCE
+export async function redirectToAuthCodeFlow(clientId) {
+  const verifier = generateCodeVerifier(128);
+  const challenge = await generateCodeChallenge(verifier);
+
+  localStorage.setItem("verifier", verifier);
+
+  // Construct the authorisation URL
+  const params = new URLSearchParams();
+  params.append("client_id", clientId);
+  params.append("response_type", "code");
+  params.append("redirect_uri", "http://localhost:5173/");
+  params.append("scope", "user-top-read user-read-private user-read-email");
+  params.append("code_challenge_method", "S256");
+  params.append("code_challenge", challenge);
+
+  // Redirect to Spotify Authorisation
+  document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
+}
+
+// Generates random string of characters for code verifier.
+function generateCodeVerifier(length) {
+  let text = "";
+  let possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < length; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
+// Takes code verifier and generate a code challenge using SHA-256 hashing algorithm.
+async function generateCodeChallenge(codeVerifier) {
+  const data = new TextEncoder().encode(codeVerifier);
+  const digest = await window.crypto.subtle.digest("SHA-256", data);
+  return btoa(String.fromCharCode.apply(null, [...new Uint8Array(digest)]))
+    .replace(/\+/g, "-")
+    .replace(/\//g, "_")
+    .replace(/=+$/, "");
 }
 
 async function getAccessToken(clientId, code) {
